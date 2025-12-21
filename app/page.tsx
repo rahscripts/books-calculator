413a5182ccf5e8a5495fc41a44fd1c42d8be47df
rahscripts <rahmantjis@gmail.com>
2025-12-21 16:35:29 +0530

    90% books calculator done.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
 'use client';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
   const [pages, setPages] = useState(0);
   const [books, setBooks] = useState(0);
-  console.log(pages);
+  
+  useEffect(() => {
+    const totalPageInYear = pages*365;
+    const totalBooksInYear = Math.round(totalPageInYear/320);
+    setBooks(totalBooksInYear);
+  }, [pages])
   return (
-    <section>
+    <section className="max-w-2xl mx-auto p-20 text-center space-y-8 bg-green-200 h-full">
+      <div>
+        <h1 className="text-3xl font-bold ">Books Calculator <span className="uppercase text-xs italic tracking-tighter decoration-wavy underline text-green-700 font-bold ">by mr</span></h1>
+      </div>
       <div>If i read
         <select onChange={(e) => setPages(Number(e.target.value))} name="bookpage" id="">
           <option value="1">1</option>
@@ -15,7 +23,8 @@ export default function Home() {
           <option value="7">7</option>
           <option value="10">10</option>
           <option value="15">15</option>
-        </select> pages everyday. By the end of the year i will read {books} books.
+        </select> pages everyday. <br />
+        By the end of the year <span className="text-1xl text-green-700 font-black">i will read {books} books</span>.
       </div>
       <div></div>
     </section>
