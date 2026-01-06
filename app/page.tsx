71fcaf13fb2518183dfe50edca89b90688a73cff
rahscripts <rahmantjis@gmail.com>
2026-01-07 01:57:35 +0530

    add months

--- app/page.tsx
+++ app/page.tsx
@@ -4,32 +4,42 @@ import Footer from "./components/Footer";
 
 
 export default function Home() {
-  const [pages, setPages] = useState(0);
+  const [pages, setPages] = useState(7);
 
-  
+  const [month, setMonth] = useState(6);
+
   const [books, setBooks] = useState(0);
 
   useEffect(() => {
-    const totalPageInYear = pages * 365;
-    const totalBooksInYear = Math.round(totalPageInYear / 320);
-    setBooks(totalBooksInYear);
-  }, [pages])
+    const totalPageInMonth = pages * 30; //300
+    const totalPageInMonthss = totalPageInMonth * month; //900
+
+    const totalBooks = Math.round(totalPageInMonthss / 320); //2 3
+    setBooks(totalBooks);
+  }, [pages, month])
   return (
     <section className="max-w-2xl mx-auto p-20 min-h-screen text-center space-y-8  h-full">
       <div>
         <h1 className="text-3xl font-bold ">Books Calculator <span className="uppercase text-xs italic tracking-tighter decoration-wavy underline text-green-700 font-bold ">by mr</span></h1>
       </div>
       <div>If i read
-        <select onChange={(e) => setPages(Number(e.target.value))} name="bookpage" className="select w-fit mx-2 select-success" id="">
+        <select defaultValue={7} onChange={(e) => setPages(Number(e.target.value))} name="bookpage" className="select w-fit mx-2 cursor-pointer select-success" id="">
           <option value="1">1</option>
           <option value="5">5</option>
           <option value="7">7</option>
           <option value="10">10</option>
           <option value="15">15</option>
-        </select> <span className="font-bold">pages</span> everyday. <br />
-        <div className="mt-8 flex flex-col -space-y-2">
-          <div>By the end of the year</div>
-           <div className="text-3xl"><span className="text-1xl text-green-700 font-black">i will read <span className="text-2xl text-red-700 underline ">{books} books</span></span>.
+        </select> <span className="font-bold">pages</span> everyday 
+        for the next  
+        <select defaultValue={6} onChange={(e) => setMonth(Number(e.target.value))} name="bookpage" className="select w-fit mx-2 cursor-pointer select-success" id="">
+          <option value="1">3</option>
+          <option value="6">6</option>
+          <option value="12">12</option>
+          <option value="24">24</option>
+        </select> months. <br />
+        <div className="mt-8 flex gap-2">
+          <div className=" italic ">By the end</div>
+           <div className="text-3xl"><span className="text-1xl text-green-700 font-black">i will read <span className="text-5xl text-shadow-md text-red-700 underline ">{books} books 📚</span></span>.
            </div>
            </div>
       </div>
