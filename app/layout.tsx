35fb3164fdc7b76ec2a2fdca81e73c8895c5b1b3
rahscripts <rahmantjis@gmail.com>
2026-01-12 16:12:34 +0530

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
+
+
 export const metadata: Metadata = {
   title: "booksofme",
   description: "Showcase your reading journey.",
@@ -15,6 +18,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-NVSX381XME"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-NVSX381XME');
+    `}
+        </Script>
+      </head>
+
       <body className="antialiased tracking-tight">
         <Providers>
           <Toaster />
